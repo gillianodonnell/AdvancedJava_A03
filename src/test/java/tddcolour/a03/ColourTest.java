70d115a6679a17d16e93f81ffac59f120388dc4b
gillianodonnell <gmb.odonnell@gmail.com>
2022-11-22 17:37:36 +0000

    Test failing as no getter for green

--- src/test/java/tddcolour/a03/ColourTest.java
+++ src/test/java/tddcolour/a03/ColourTest.java
@@ -9,9 +9,16 @@ public class ColourTest {
 
     @Test
     @DisplayName("Check that red parameter is a float")
-    void newColourRedisFloat(){
+    void newColourRedIsFloat(){
         Colour testColour = new Colour(0.81f,0.52f,0.43f);
         assertEquals(0.81f, testColour.getRed());
+    }
+
+    @Test
+    @DisplayName("Check that green parameter is a float")
+    void newColourGreenIsFloat(){
+        Colour testColour = new Colour(0.81f,0.52f,0.43f);
+        assertEquals(0.52f, testColour.getGreen());
     }
 
 }
