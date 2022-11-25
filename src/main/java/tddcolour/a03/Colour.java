0236fcc74bf4eb0ac8541f2ba5e03a55dcc3f759
gillianodonnell <gmb.odonnell@gmail.com>
2022-11-25 13:01:25 +0000

    Test passed for checking red input between 0.0f and 1.0f

--- src/main/java/tddcolour/a03/Colour.java
+++ src/main/java/tddcolour/a03/Colour.java
@@ -1,15 +1,23 @@
 package tddcolour.a03;
 
 
+import java.lang.reflect.Executable;
+
 public class Colour {
     private final float red;
     private final float green;
     private final float blue;
 
-    public Colour(float red, float green, float blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
+    public Colour(float red, float green, float blue) throws IllegalArgumentException{
+
+        //test if red float input is between 0.0 and 1.0
+        if(red < 0.0f || red > 1.0f){
+            throw new IllegalArgumentException("Red input must be between 0.0f and 1.0f");
+        }else{
+            this.red = red;
+        }
+
+
     }
 
     public float getRed() {
