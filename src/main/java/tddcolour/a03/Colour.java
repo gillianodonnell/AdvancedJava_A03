ef2ff91afbd247b3581f5c46d7a7192546017339
gillianodonnell <gmb.odonnell@gmail.com>
2022-11-29 15:20:34 +0000

    Java doc for Colour Class

--- src/main/java/tddcolour/a03/Colour.java
+++ src/main/java/tddcolour/a03/Colour.java
@@ -3,6 +3,18 @@ package tddcolour.a03;
 
 import java.lang.String;
 
+/**
+Colour class representing RGB colours.
+The Colour class implements the following functionality
+ • A constructor for a Colour object with three parameters which specify the colour components, in
+ the order red, green and blue, using a floating point values in the range 0.0 to 1.0
+• A constructor for a Colour object that specifies one parameter which is a combined RGB value
+consisting of the red component in bits 16-23, the green component in bits 8-15, and the blue
+component in bits 0-7
+• It is not allowed to create a Colour object without specifying valid component values
+• Colours can be compared, to be equal they must have the same component values
+ */
+
 public class Colour {
     private float red;
     private float green;
@@ -16,6 +28,13 @@ public class Colour {
     private float redBitDecimal;
 
 
+    /**
+     * A constructor for a Colour object with three parameters which specify the colour components
+     * @param red red colour floating point value in the range 0.0 to 1.0
+     * @param green green colour floating point value in the range 0.0 to 1.0
+     * @param blue blue floating point value in the range 0.0 to 1.0
+     * @throws IllegalArgumentException exception thrown to indicate an illegal or unsuitable argument passed to a method
+     */
     public Colour(float red, float green, float blue) throws IllegalArgumentException {
         //test if red float input is between 0.0 and 1.0
         if (red < 0.0f || red > 1.0f) {
@@ -39,6 +58,15 @@ public class Colour {
 
     }
 
+    /**
+     * A constructor for a Colour object that specifies one parameter which is a combined RGB value
+     * consisting of the red component in bits 16-23, the green component in bits 8-15, and the blue
+     * component in bits 0-7
+     * @param combinedRGB one String parameter which is a combined RGB value
+     * consisting of the red component in bits 16-23, the green component in bits 8-15, and the blue
+     * component in bits 0-7
+     * @throws IllegalArgumentException exception thrown to indicate an illegal or unsuitable argument passed to a method
+     */
     public Colour(String combinedRGB) throws IllegalArgumentException {
         //checks that length of rgb is 24
         if (combinedRGB.length() != 24) {
@@ -58,44 +86,88 @@ public class Colour {
         this.redBitDecimal = convertBinaryToFloat(redBit);
     }
 
+    /**
+     * Getter for 3 parameter Colour constructor
+     * @return red colour floating point value in the range 0.0 to 1.0
+     */
     public float getRed() {
         return red;
     }
 
+    /**
+     * Getter for 3 parameter Colour constructor
+     * @return green colour floating point value in the range 0.0 to 1.0
+     */
     public float getGreen() {
         return green;
     }
 
+    /**
+     * Getter for 3 parameter Colour constructor
+     * @return blue colour floating point value in the range 0.0 to 1.0
+     */
     public float getBlue() {
         return blue;
     }
 
+    /**
+     * Getter for 3 parameter Colour constructor
+     * @return combinedRGB one parameter which is a combined RGB value
+     * consisting of the red component in bits 16-23, the green component in bits 8-15, and the blue
+     * component in bits 0-7
+     */
     public String getCombinedRGB() {
         return combinedRGB;
     }
 
+    /**
+     * Getter for 1 parameter Colour constructor
+     * @return string blue component in bits 0-7
+     */
     public String getBlueBit() {
         return blueBit;
     }
 
+    /**
+     * Getter for 1 parameter Colour constructor
+     * @return string blue component in bits 8-15
+     */
     public String getGreenBit() {
         return greenBit;
     }
 
+    /**
+     * Getter for 1 parameter Colour constructor
+     * @return string blue component in bits 16-24
+     */
     public String getRedBit() {
         return redBit;
     }
 
+    /**
+     * Method converting binary string to float
+     * @param binaryString string of bits
+     * @return float
+     */
     public float convertBinaryToFloat(String binaryString) {
         float numerator = Integer.parseInt(binaryString, 2);
         float decimal = numerator / 255;
         return decimal;
     }
 
+    /**
+     * Getter for 1 parameter Colour constructor
+     * @return float red component bits 0-7
+     */
     public float getBlueBitDecimal() {
         return blueBitDecimal;
     }
 
+    /**
+     * Method to check that bit values in string are only 1s or 0s
+     * @param bitString string of bits
+     * @return boolean
+     */
     boolean validBit(String bitString) {
         char[] ch = bitString.toCharArray();
         for(int i = 0; i < ch.length; i++) {
@@ -106,14 +178,29 @@ public class Colour {
         return false;
     }
 
+    /**
+     * Getter for 1 parameter Colour constructor
+     * @return float green component bits 8-15
+     */
     public float getGreenBitDecimal() {
         return greenBitDecimal;
     }
 
+    /**
+     * Getter for 1 parameter Colour constructor
+     * @return float red component bits 16-24
+     */
     public float getRedBitDecimal() {
         return redBitDecimal;
     }
 
+    /**
+     *
+     * @param blueBit blue float value between 0.0 and 1.0
+     * @param greenBit green float value between 0.0 and 1.0
+     * @param redBit red float value between 0.0 and 1.0
+     * @return float product - combined rgb
+     */
     public float makeColour(float blueBit, float greenBit, float redBit){
         return blueBit * greenBit * redBit;
     }
